7ecdcccab8cb28f6b8056d5c84b1b935db1b7d67
Albert Gergo <gergoalbert.gta@gmail.com>
2020-10-03 21:12:52 +0300

    Removed Passport

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,33 +2,20 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import cookieSession from "cookie-session";
-import cookieParser from "cookie-parser";
 import passport from "passport";
 import dotenv from "dotenv";
 import { notFoundHandler, errorHandler } from "./middlewares/middlewares";
 import api from "./routes/api/api";
 import auth from "./routes/auth/auth";
-import passportConfig from "./passportConfig/config";
 
 dotenv.config();
 
 const app = express();
 
-passportConfig();
-
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-app.use(
-  cookieSession({
-    name: "auth_session",
-    keys: [process.env.SESSION_KEY!],
-    httpOnly: true,
-  })
-);
-app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/api/v1", api);
--- server/src/routes/auth/auth.ts
+++ server/src/routes/auth/auth.ts
@@ -1,15 +1,7 @@
 import { Router } from "express";
-import passport from "passport";
 
 const router = Router();
 
-router.get("/github", passport.authenticate("github"));
-router.get(
-  "/github/callback",
-  passport.authenticate("github", {
-    successRedirect: "http://localhost:3000/",
-  })
-);
 router.get("/", (req, res) => {
   res.send("hello, world");
 });
