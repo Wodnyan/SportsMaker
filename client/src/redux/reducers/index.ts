53333c39689a4d770f991691d70d6f3900cae685
Albert Gergo <gergoalbert.gta@gmail.com>
2020-10-11 21:47:15 +0300

    Entries State

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import filters from "./filter";
+import entries from "./entries";
 
-export default combineReducers({ user, filters });
+export default combineReducers({ user, filters, entries });
